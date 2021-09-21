72dfbfecf54bca102bbe7f7de609717702ecb2a9
Andrea Marangon <marangonandrea84@gmail.com>
2021-09-21 17:49:30 +0200

    aggiunto filtro che fa vedere i primi 5 suggerimenti e al click su "mostra tutti" fa vedere i restanti

--- src/components/Suggestions.vue
+++ src/components/Suggestions.vue
@@ -2,28 +2,59 @@
   <div id="suggestions">
     <div class="d-flex justify-content-between">
       <span class="my-lightgray my-bold">Suggerimenti per te</span>
-      <span class="my-font-size-12 my-bold">Mostra tutti</span>
+      <span
+        @click="(showLess = false), (isHidden = true)"
+        v-if="!isHidden"
+        class="my-font-size-12 my-text-bold"
+        >Mostra tutti</span
+      >
     </div>
-    <div
-      v-for="(profile, index) in profiles"
-      :key="index"
-      class="profile-container d-flex justify-content-between mt-2"
-    >
-      <div class="d-flex">
-        <img
-          :src="profile.profile_picture"
-          class="align-self-center rounded-circle me-3"
-          alt="immagine-profilo"
-        />
-        <div class="profile-name d-flex flex-column justify-content-center">
-          <span class="my-text-bold">{{ profile.profile_name }}</span>
-          <span class="my-lightgray my-font-size-12"
-            >Consigliati da Instagram</span
-          >
+    <div v-if="showLess">
+      <div
+        v-for="(profile, index) in profiles.slice(0, 5)"
+        :key="index"
+        class="profile-container d-flex justify-content-between mt-2"
+      >
+        <div class="d-flex">
+          <img
+            :src="profile.profile_picture"
+            class="align-self-center rounded-circle me-3"
+            alt="immagine-profilo"
+          />
+          <div class="profile-name d-flex flex-column justify-content-center">
+            <span class="my-text-bold">{{ profile.profile_name }}</span>
+            <span class="my-lightgray my-font-size-12"
+              >Consigliati da Instagram</span
+            >
+          </div>
+        </div>
+        <div class="d-flex align-items-center">
+          <span class="my-lightblue my-font-size-12 my-bold">Segui</span>
         </div>
       </div>
-      <div class="d-flex align-items-center">
-        <span class="my-lightblue my-font-size-12 my-bold">Segui</span>
+    </div>
+    <div v-else>
+      <div
+        v-for="(profile, index) in profiles"
+        :key="index"
+        class="profile-container d-flex justify-content-between mt-2"
+      >
+        <div class="d-flex">
+          <img
+            :src="profile.profile_picture"
+            class="align-self-center rounded-circle me-3"
+            alt="immagine-profilo"
+          />
+          <div class="profile-name d-flex flex-column justify-content-center">
+            <span class="my-text-bold">{{ profile.profile_name }}</span>
+            <span class="my-lightgray my-font-size-12"
+              >Consigliati da Instagram</span
+            >
+          </div>
+        </div>
+        <div class="d-flex align-items-center">
+          <span class="my-lightblue my-font-size-12 my-bold">Segui</span>
+        </div>
       </div>
     </div>
   </div>
@@ -33,6 +64,12 @@
 export default {
   name: "Suggestions",
   props: ["profiles"],
+  data() {
+    return {
+      showLess: true,
+      isHidden: false,
+    };
+  },
 };
 </script>
 
